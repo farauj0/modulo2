b3b9e7e279de0b446c5d79cc0881296c3572ea80
farauj0 <fernando.araujo@sou.inteli.edu.br>
2022-06-20 20:10:59 -0300

    corrigido exercício 5 - semana 9

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/exercicio_5/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/exercicio_5/script.js
@@ -3,29 +3,18 @@ function primeNumbers() {
     const num2 = document.getElementById('num2').value
     var primes = []
     for (let i = 0; i<=num2;i++) {
-        var m = false
-        if (i>=num1) {
-            if (i == 2) {
-                var m = true
-              } else if (i > 1) {
-                for (var j = 2; j < i; j++) {
-            
-                  if (i % j != 0) {
-                    var m = true;
-                  } else if (i == j * j) {
-                    var m = false
-                  } else {
-                    var m = false;
-                  }
-                }
-              } else {
-                var m = false;
-              }
-            if (m == true) {
-                primes.push(i)
-            }
-            
-            }
+      let isPrime = true
+      if (i > 1) {
+        for (let j = 2; j < i; j++) {
+          if (i % j == 0) {
+              isPrime = false;
+              break;
+          }
+      }
+      if (isPrime) {
+        primes.push(i);
+    }
+      }
     }
     document.getElementById('output').innerText = primes
     // document.getElementById('output').innerText += 'oi'
